611616c8da993310d6b5111184f4e2c3f8616890
kulinitpl <kulinrangani01@gmail.com>
2023-09-20 17:36:31 +0530

    context

--- src/pages/LiveAlerts/LiveAlerts.js
+++ src/pages/LiveAlerts/LiveAlerts.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from "react";
-import { WebSocketContext, useWebSocket } from "../../context/WebSocketContext";
+import { WebSocketContext } from "../../context/WebSocketContext";
 
 export default function LiveAlerts() {
   let socket = React.useContext(WebSocketContext) 
-//   useWebSocket();
 
   useEffect(() => {
+    console.log("Live Alerts",WebSocketContext);
     if (socket) {
       socket.addEventListener("message", (event) => {
         console.log("Received message:", event.data);
       });
-      console.log(socket);
     }
+    console.log(socket);
   }, [socket]);
   return <div>LiveAlerts</div>;
 }
