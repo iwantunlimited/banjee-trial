a7a7323306c747b66a7b4e2e746d7289bbfcd5bc
harshpatel1491 <harsh.patel@itpl.io>
2023-11-24 17:39:28 +0530

    changed condition for create thought button

--- src/pages/Thoughts/Thoughts.js
+++ src/pages/Thoughts/Thoughts.js
@@ -17,13 +17,15 @@ import ThoughtCard from "./Components/ThoughtCard";
 import DeleteThoughtModal from "./Components/DeleteThoughtModal";
 import { deleteThought, getThoughts } from "./services/ApiServices";
 export default function Thoughts() {
-	const { themeData, setModalData, setModalOpen } = React.useContext(MainContext);
+	const { themeData, setModalData, setModalOpen } =
+		React.useContext(MainContext);
 	const theme = useTheme();
 	const [thoughts, setThoughts] = useState();
 	const [openCreateModal, setOpenCreateModal] = React.useState(false);
 	const [deleteModal, setDeleteModal] = React.useState(false);
 	const [deleteThoughtId, setDeleteThoughtId] = React.useState("");
 	const [loading, setLoading] = useState(false);
+	const [adminThought, setAdminThought] = useState(true);
 	const [pagination, setPagination] = React.useState({
 		page: 0,
 		pageSize: 10,
@@ -39,6 +41,11 @@ export default function Thoughts() {
 			.then((res) => {
 				setThoughts(res.content);
 				setTotalEle(res?.totalElements);
+				res?.content?.map((ele) => {
+					if (ele?.user?.userType === -1) {
+						setAdminThought(false);
+					}
+				});
 			})
 			.finally(() => {
 				setLoading(false);
@@ -51,6 +58,7 @@ export default function Thoughts() {
 				getThoughtsApiCall();
 				setModalOpen(true);
 				setModalData("Thought Deleted Successfully", "success");
+				setAdminThought(true);
 			})
 			.catch((err) => {
 				console.log("====================================");
@@ -72,7 +80,8 @@ export default function Thoughts() {
 					display: "flex",
 					justifyContent: "center",
 					alignItems: " center",
-				}}>
+				}}
+			>
 				<CircularProgress />
 			</Box>
 		);
@@ -87,29 +96,32 @@ export default function Thoughts() {
 						justifyContent: "space-between",
 						alignItems: "center",
 						flexDirection: { xs: "column", sm: "row" },
-					}}>
+					}}
+				>
 					<Box
 						sx={{
 							marginBottom: { xs: "20px", sm: "0px" },
 							display: "flex",
 							justifyContent: "space-between",
 							alignItems: "center",
-						}}>
+						}}
+					>
 						<Typography
 							sx={{
 								color: themeData ? "default" : "#6b778c",
 								fontSize: "22px",
 								fontWeight: "500",
 								textAlign: "left",
-							}}>
+							}}
+						>
 							Thoughts
 							{/* ({thoughts?.length}) */}
 						</Typography>
 					</Box>
 
-					{thoughts?.length === 0 && (
+					{adminThought && (
 						<Box sx={{ marginLeft: "10px" }}>
-							<Tooltip title='Create Thougth'>
+							<Tooltip title="Create Thougth">
 								<IconButton
 									onClick={() => {
 										setOpenCreateModal(true);
@@ -119,7 +131,8 @@ export default function Thoughts() {
 										background: theme?.palette?.primary.main,
 										padding: window.innerWidth < 501 ? "5px" : "10px",
 										color: theme?.palette?.primary.contrastText,
-									}}>
+									}}
+								>
 									<Add />
 								</IconButton>
 							</Tooltip>
@@ -139,27 +152,46 @@ export default function Thoughts() {
 					/>
 				</Card>
 				{thoughts.length > 0 ? (
-					<Grid container spacing={2}>
+					<Grid
+						container
+						spacing={2}
+					>
 						{thoughts?.map((ele, index) => {
 							return (
-								<Grid item xs={12} sm={12} md={12} lg={6} xl={4} key={index}>
-									<ThoughtCard ele={ele} setOpen={setDeleteModal} setDeleteThoughtId={setDeleteThoughtId} />
+								<Grid
+									item
+									xs={12}
+									sm={12}
+									md={12}
+									lg={6}
+									xl={4}
+									key={index}
+								>
+									<ThoughtCard
+										ele={ele}
+										setOpen={setDeleteModal}
+										setDeleteThoughtId={setDeleteThoughtId}
+									/>
 								</Grid>
 							);
 						})}
-						<Grid item xs={12}>
+						<Grid
+							item
+							xs={12}
+						>
 							{/* <Card> */}
 							{/* pagination for all feeds */}
 							<Box
-								className='root'
+								className="root"
 								sx={{
 									"& > div > div": {
 										display: "flex",
 										alignItems: "baseline !important",
 									},
-								}}>
+								}}
+							>
 								<TablePagination
-									component='div'
+									component="div"
 									count={totalEle}
 									page={pagination.page}
 									rowsPerPage={pagination.pageSize}
@@ -187,7 +219,8 @@ export default function Thoughts() {
 							display: "flex",
 							justifyContent: "center",
 							alignItems: " center",
-						}}>
+						}}
+					>
 						<Typography>No Data Found !</Typography>
 					</Box>
 				)}
@@ -202,7 +235,8 @@ export default function Thoughts() {
 					display: "flex",
 					justifyContent: "center",
 					alignItems: " center",
-				}}>
+				}}
+			>
 				<CircularProgress />
 			</Box>
 		);
